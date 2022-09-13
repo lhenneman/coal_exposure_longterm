38e52f65e9d40bee6cd510880a734c66d5157309
lhenneman <lhenneman@gmail.com>
2022-09-13 15:06:07 -0400

    update file path to hyads

--- code/coal_pm25_to_county.R
+++ code/coal_pm25_to_county.R
@@ -19,7 +19,7 @@ library( magrittr)
 ## ============================================================
 # gridded hyads file location
 hyads_file_loc <- './data/inputs/coal_pm25/'
-hyads_file_loc <- '~/Dropbox/Harvard/ARP/HyADS/hyads_longterm/exp_pm25_new/grids'
+hyads_file_loc <- '~/Dropbox/Harvard/ARP/HyADS/hyads_longterm/exp_pm25_noint/grids_model.lm.cv_single_poly'
 
 #coordinate reference system projection string for spatial data
 p4s <- "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +a=6370000 +b=6370000"
